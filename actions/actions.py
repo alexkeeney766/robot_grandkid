ef21c68ddf9dddfad19b471c330741480c39dbb4
alexkeeney766 <alex.keeney766@gmail.com>
2022-01-11 19:10:30 -0500

    custom action+new training data

--- actions/actions.py
+++ actions/actions.py
@@ -1,28 +1,27 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
+from typing import Any, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
 
 
-# This is a simple example for a custom action which utters "Hello World!"
+docs_lookup = {
+    'PC': 'https://www.microsoft.com/en-us/windows/getting-started?r=1', 
+    'Mac': 'https://help.apple.com/macOS/high-sierra/mac-basics/#/intro'
+}
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
-pass
+responses_lookup = {
+    'PC': f"Here is Microsoft's the Getting Started Guide for your PC: {docs_lookup['PC']}",
+    'Mac': f"Here is Apple's the Mac-Basics Guide for your Mac: {docs_lookup['Mac']}",
+}
+
+class ActionDocsSelector(Action):
+    def name(self) -> str:
+        return "action_docs_selector"
+
+    def run(
+        self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[str, Any]
+    ) -> List[Dict[str, Any]]:
+        
+        dispatcher.utter_message(text=responses_lookup[tracker.get_slot('system')])
+
+        return []
