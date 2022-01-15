2d58306671ef5854dc22c27cda27134eb41569e6
alexkeeney766 <alex.keeney766@gmail.com>
2022-01-14 19:02:22 -0500

    Two interfaces and refactor

---
+++ react-interface/src/App.js
@@ -0,0 +1,35 @@
+import { React, useState } from 'react';
+import Widget from 'rasa-webchat';
+import './App.css';
+
+
+
+
+function App() {
+  const [cleared, setCleared] = useState(false);
+
+  const resetConversation = () => {
+    console.log('reseting local storage');
+    localStorage.clear();
+    setCleared(true);
+  };
+
+  return (
+    <div className="App">
+      <div>
+        <button onClick={resetConversation}>Restart</button>
+      </div>
+      <Widget
+        socketUrl={"http://localhost:5005"}
+        socketPath={"/socket.io/"}
+        title={"Robot Grandkid"}
+        // embedded={true}
+        showFullScreenButton={true}
+        subtitle={'Ask me about your tech problems'}
+        storage={'local'}
+      />
+    </div>
+  );
+}
+
+export default App;
